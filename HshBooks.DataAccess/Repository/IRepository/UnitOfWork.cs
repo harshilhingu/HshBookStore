6d31e9677428b655255b3c490c9cd9a8d065cd57
harshilhingu <91302645+harshilhingu@users.noreply.github.com>
2022-03-29 16:39:17 -0400

    completed

---
+++ HshBooks.DataAccess/Repository/IRepository/UnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HshBooks.DataAccess.Repository.IRepository
+{
+    interface UnitOfWork : IDisposable
+
+    {
+        ICategoryRepository Category { get; }
+        ISP_Call SP_Call { get; }
+    }
+}
---
+++ HshBooks.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿
+using HshBooks.DataAccess.Repository.IRepository;
+using HshBookStore.DataAccess.Data;
+using HshBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HshBooks.DataAccess.Repository
+{
+    public class UnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+            SP_Call = new SP_Call(_db);
+        }
+        public ICategoryRepository Category { get; private set; }
+        public ISP_Call SP_Call { get; private set; }
+
+    }
+}
